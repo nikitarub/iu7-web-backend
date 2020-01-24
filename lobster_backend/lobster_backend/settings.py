8dfadeb00566003e2c041aa4238c6a0d50dd4ff7
Nikita Rub <nikita.rub@yandex.ru>
2020-01-24 09:19:19 +0300

    Moved to PostgreSQL succsessfully

--- lobster_backend/lobster_backend/settings.py
+++ lobster_backend/lobster_backend/settings.py
@@ -96,8 +96,10 @@ WSGI_APPLICATION = 'lobster_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.path.join(BASE_DIR, 'postgresql'),
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
